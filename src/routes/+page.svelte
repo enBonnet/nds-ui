45015e4c02f627658f013eff4e8b8b765137cca5
Ender Bonnet <13243693+enBonnet@users.noreply.github.com>
2023-02-21 20:13:34 +0000

    [exammples]: bottons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <script>
-  import "../lib/styles/base.css"
-  import Button from "../lib/button.svelte";
+	import '../lib/styles/base.css';
+	import Button from '../lib/button.svelte';
 </script>
 
 <div>
-  Buttons
-  <div>
-    <Button>Button</Button>
-    <Button size="sm" color="green">Button</Button>
-    <Button size="sm" color="red">Button</Button>
-    <Button size="sm" color="blue">Button</Button>
-  </div>
+	Buttons
+	<div>
+		<div>
+			<Button>Button</Button>
+			<Button color="green">Button</Button>
+			<Button color="red">Button</Button>
+			<Button color="blue">Button</Button>
+		</div>
+	</div>
 </div>
