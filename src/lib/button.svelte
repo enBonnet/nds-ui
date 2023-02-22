f8e5083496c1670bfae814b87c7eb759e0036775
Ender Bonnet <enbonnet@gmail.com>
2023-02-22 18:02:55 -0300

    [buttons]: support anchor and replace px to rem

--- src/lib/button.svelte
+++ src/lib/button.svelte
@@ -1,100 +1,114 @@
 <script lang="ts">
-  import classnames from "classnames";
-  export let size: "sm" | "md" | "lg" | "xl" = "md";
-  export let color: "green" | "red" | "blue" | "base" = "base";
-  const cn = classnames("button", size, color);
+	import cn from 'classnames';
+	export let size: 'sm' | 'md' | 'lg' | 'xl' = 'md';
+	export let color: 'green' | 'red' | 'blue' | 'base' = 'base';
+	export let disabled: boolean = false;
+	export let loading: boolean = false;
+	export let href: string | undefined = undefined;
+	export let tabindex: number = 0;
+  export let external: boolean = false;
+	let className: string = '';
+	export { className as class };
+	const classes = cn('button', className, size, color, { loading, disabled });
 </script>
 
-<button class={cn}>
-  <slot />
-</button>
+{#if href && !disabled}
+	<a {href} {tabindex} role="button" rel="noreferrer noopener" target={external ? '_blank' : '_self'} class={classes} {...$$restProps}>
+		<slot />
+	</a>
+{:else}
+	<button on:click {tabindex} {disabled} class={classes} {...$$restProps}>
+		<slot />
+	</button>
+{/if}
 
 <style>
-  /* base */
-  .button {
-    position: relative;
-    font-weight: 700;
-    overflow: hidden;
-    margin-top: 10px;
-    font-size: inherit;
-    top: -8px;
-    border-radius: 10px;
-    color: #050505;
-  }
-  /* customizations */
-  .button.sm {
-    padding: 8px 16px;
-  }
-  .button.md {
-    padding: 11px 22px;
-  }
-  .button.lg {
-    padding: 14px 28px;
-    font-size: 22px;
-  }
-  .button.xl {
-    padding: 17px 34px;
-    font-size: 28px;
-  }
-  .button.green {
-    border: solid 2px #165a38;
-    background: #67daa2;
-    box-shadow: 0 6px 0 0#165a38;
-  }
-  .button.green:hover {
-    box-shadow: 0 4px 0 0#165a38,0 4px 4px 0 rgba(0,0,0,.4);
-  }
-  .button.green:active {
-    top: 0;
-    box-shadow: 0 0 0 0#165a38;
-  }
-  .button.red {
-    border: solid 2px #5e1817;
-    background: #df7d7c;
-    box-shadow: 0 6px 0 0#5e1817;
-  }
-  .button.red:hover {
-    box-shadow: 0 4px 0 0#5e1817,0 4px 4px 0 rgba(0,0,0,.4);
-  }
-  .button.red:active {
-    top: 0;
-    box-shadow: 0 0 0 0#5e1817;
-  }
-  .button.blue {
-    border: solid 2px #16345a;
-    background: #679fda;
-    box-shadow: 0 6px 0 0#16345a;
-  }
-  .button.blue:hover {
-    box-shadow: 0 4px 0 0#16345a,0 4px 4px 0 rgba(0,0,0,.4);
-  }
-  .button.blue:active {
-    top: 0;
-    box-shadow: 0 0 0 0#16345a;
-  }
-  .button.base {
-    border: solid 2px #8d8a86;
-    background: #c3c2c0;
-    box-shadow: 0 6px 0 0#8d8a86;
-  }
-  .button.base:hover {
-    box-shadow: 0 4px 0 0#8d8a86,0 4px 4px 0 rgba(0,0,0,.4);
-  }
-  .button.base:active {
-    top: 0;
-    box-shadow: 0 0 0 0#8d8a86;
-  }
-  /* status */
-  .button:hover {
-    cursor: pointer;
-    top: -4px;
-  }
-  .button:focus {
-    outline: solid 4px #d8b600;
-    background: #ffd90c;
-    outline-offset: -6px;
-    color: #050505;
-    text-decoration: none;
-    opacity: 1;
-  }
+	/* base */
+	.button {
+		text-decoration: none;
+		position: relative;
+		font-weight: 700;
+		overflow: hidden;
+		margin-top: 0.625rem;
+		font-size: inherit;
+		top: -0.5rem;
+		border-radius: 0.625rem;
+		color: #050505;
+	}
+	/* customizations */
+	.button.sm {
+		padding: 0.5rem 1rem;
+	}
+	.button.md {
+		padding: 0.6875rem 1.375rem;
+	}
+	.button.lg {
+		padding: 0.875rem 1.75rem;
+		font-size: 1.375rem;
+	}
+	.button.xl {
+		padding: 1.0625rem 2.125rem;
+		font-size: 1.75rem;
+	}
+	.button.green {
+		border: solid 0.125rem #165a38;
+		background: #67daa2;
+		box-shadow: 0 0.375rem 0 0#165a38;
+	}
+	.button.green:hover {
+		box-shadow: 0 0.25rem 0 0#165a38, 0 0.25rem 0.25rem 0 rgba(0, 0, 0, 0.4);
+	}
+	.button.green:active {
+		top: 0;
+		box-shadow: 0 0 0 0#165a38;
+	}
+	.button.red {
+		border: solid 0.125rem #5e1817;
+		background: #df7d7c;
+		box-shadow: 0 0.375rem 0 0#5e1817;
+	}
+	.button.red:hover {
+		box-shadow: 0 0.25rem 0 0#5e1817, 0 0.25rem 0.25rem 0 rgba(0, 0, 0, 0.4);
+	}
+	.button.red:active {
+		top: 0;
+		box-shadow: 0 0 0 0#5e1817;
+	}
+	.button.blue {
+		border: solid 0.125rem #16345a;
+		background: #679fda;
+		box-shadow: 0 0.375rem 0 0#16345a;
+	}
+	.button.blue:hover {
+		box-shadow: 0 0.25rem 0 0#16345a, 0 0.25rem 0.25rem 0 rgba(0, 0, 0, 0.4);
+	}
+	.button.blue:active {
+		top: 0;
+		box-shadow: 0 0 0 0#16345a;
+	}
+	.button.base {
+		border: solid 0.125rem #8d8a86;
+		background: #c3c2c0;
+		box-shadow: 0 0.375rem 0 0#8d8a86;
+	}
+	.button.base:hover {
+		box-shadow: 0 0.25rem 0 0#8d8a86, 0 0.25rem 0.25rem 0 rgba(0, 0, 0, 0.4);
+	}
+	.button.base:active {
+		top: 0;
+		box-shadow: 0 0 0 0#8d8a86;
+	}
+	/* status */
+	.button:hover {
+		cursor: pointer;
+		top: -0.25rem;
+	}
+	.button:focus {
+		outline: solid 0.25rem #d8b600;
+		background: #ffd90c;
+		outline-offset: -0.375rem;
+		color: #050505;
+		text-decoration: none;
+		opacity: 1;
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,29 +6,11 @@
 <div>
 	Buttons
 	<div class="buttons">
-		<div class="sm">
-			<Button size="sm">Button</Button>
-			<Button size="sm" color="green">Button</Button>
-			<Button size="sm" color="red">Button</Button>
-			<Button size="sm" color="blue">Button</Button>
-		</div>
 		<div class="md">
 			<Button>Button</Button>
 			<Button color="green">Button</Button>
 			<Button color="red">Button</Button>
 			<Button color="blue">Button</Button>
-		</div>
-		<div class="lg">
-			<Button size="lg">Button</Button>
-			<Button size="lg" color="green">Button</Button>
-			<Button size="lg" color="red">Button</Button>
-			<Button size="lg" color="blue">Button</Button>
-		</div>
-		<div class="xl">
-			<Button size="xl">Button</Button>
-			<Button size="xl" color="green">Button</Button>
-			<Button size="xl" color="red">Button</Button>
-			<Button size="xl" color="blue">Button</Button>
 		</div>
 	</div>
 </div>
